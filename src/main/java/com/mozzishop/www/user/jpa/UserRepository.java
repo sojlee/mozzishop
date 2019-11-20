489dc7a05ba151c2545c29815723daa805204290
이소정 <720sojung@gmail.com>
2019-11-20 16:38:41 +0900

    findByEmail 추가

--- src/main/java/com/mozzishop/www/user/jpa/UserRepository.java
+++ src/main/java/com/mozzishop/www/user/jpa/UserRepository.java
@@ -1,7 +1,11 @@
 package com.mozzishop.www.user.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String>{
+	
+	List<User> findByEmail();
 
 }
