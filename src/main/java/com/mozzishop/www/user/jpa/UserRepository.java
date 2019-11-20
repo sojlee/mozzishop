b17df26be6a1970202b5ebd24b0c2912f344ca12
이소정 <720sojung@gmail.com>
2019-11-20 16:51:41 +0900

    findByEmail 메소드타입을 List<User>에서 User로 변경

--- src/main/java/com/mozzishop/www/user/jpa/UserRepository.java
+++ src/main/java/com/mozzishop/www/user/jpa/UserRepository.java
@@ -1,11 +1,9 @@
 package com.mozzishop.www.user.jpa;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String>{
 	
-	List<User> findByEmail();
+	User findByEmail(String email);
 
 }
