e2e0f01af6a1fb3a4b76fd552d6e7820ce6b5de3
이소정 <720sojung@gmail.com>
2019-11-20 16:59:38 +0900

    @SocialUser, UserArgumentResolever 생성

---
+++ src/main/java/com/mozzishop/www/resolver/SocialUser.java
@@ -0,0 +1,11 @@
+package com.mozzishop.www.resolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SocialUser {
+}
