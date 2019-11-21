9e36b27b7378125778b488af072c2b3265b4e62c
이소정 <720sojung@gmail.com>
2019-11-21 14:47:37 +0900

    WebMvcConfigurer 상속, UserArgumentResolver autowired 추가, method 추

--- src/main/java/com/mozzishop/www/MozzishopApplication.java
+++ src/main/java/com/mozzishop/www/MozzishopApplication.java
@@ -1,13 +1,28 @@
 package com.mozzishop.www;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.mozzishop.www.resolver.UserArgumentResolver;
 
 @SpringBootApplication
-public class MozzishopApplication {
+public class MozzishopApplication implements WebMvcConfigurer{
+	
+	@Autowired
+	private UserArgumentResolver userArgumentResolver;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MozzishopApplication.class, args);
+	}
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(userArgumentResolver);
 	}
 
 }
