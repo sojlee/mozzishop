1d5b1ed2e813cfb32d11b55df72591b5c36f3416
이소정 <720sojung@gmail.com>
2019-11-28 10:08:38 +0900

    role update코드 추가.

--- src/main/java/com/mozzishop/www/user/jpa/UserRepository.java
+++ src/main/java/com/mozzishop/www/user/jpa/UserRepository.java
@@ -1,6 +1,12 @@
 package com.mozzishop.www.user.jpa;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,5 +15,14 @@ public interface UserRepository extends JpaRepository<User, String>{
 	User findByEmail(String email);
 
 	User findByPincipal(String pincipal);
+	
+	List<User> findAllByGrade(Grade grade);
+	
+	User findByGrade(Grade grade);
+	
+	@Modifying
+	@Transactional
+	@Query("UPDATE User SET grade = :grade WHERE idx = :idx")
+	void MakeAdmin(Grade grade, long idx);
 
 }
